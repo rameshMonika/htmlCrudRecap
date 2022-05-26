16c87370acaa49f0b0c87c635753b359f32de09b
rameshMonika <Monika.20@ichat.sp.edu.sg>
2022-05-26 19:07:30 +0800

    commit

--- firstExpressServer/model/babies.js
+++ firstExpressServer/model/babies.js
@@ -123,53 +123,58 @@ var BabiesDB = {
     //  Assignment 2
     addBaby: function (name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month, callback) {
         // geta connection to the database
+    
         var conn = db.getConnection();
 
-        conn.connect(function (err) {
-            if (err) {
-                console.log(err);
-                return callback(err.null);
-            }
-            else {
-                console.log("Connected!");
+        conn.connect(
+            function (err) {
 
+          
                 var sql = `INSERT INTO
-                baby (
-                   name,
-                   height_six_month,
-                   height_seven_month, 
-                   height_eight_month, 
-                   height_nine_month,
-                   height_ten_month)
-       VALUES
-       (
-        ?,
-         ?,
-         ?,
-         ?,
-         ?,
-         ?
-         );
-                `;
+    baby (
+       name,
+       height_six_month,
+       height_seven_month, 
+       height_eight_month, 
+       height_nine_month,
+       height_ten_month)
+VALUES
+(
+?,
+?,
+?,
+?,
+?,
+?
+);
+    `;
 
+    
                 conn.query(sql, [name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month], function (err, result) {
 
-                    conn.end();
+
                     if (err) {
-                        console.log(err);
-                        return callback(err.null);
+
+                        console.log(err.code);
+                        return callback(err);
+                        // return callback(null, result);
                     } else {
                         return callback(null, result);
+
                     }
+                    // conn.end();
+                    // return callback(null, result);
                 });
-            }
+
+
         });
+    
     },
 
 
 
 
-    updateBaby: function (name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month,id,callback) {
+    updateBaby: function (name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month, id, callback) {
         // geta connection to the database
         var conn = db.getConnection();
 
@@ -198,7 +203,7 @@ var BabiesDB = {
 
                 `;
 
-                conn.query(sql, [name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month,id], function (err, result) {
+                conn.query(sql, [name, height_six_month, height_seven_month, height_eight_month, height_nine_month, height_ten_month, id], function (err, result) {
 
                     conn.end();
                     if (err) {
